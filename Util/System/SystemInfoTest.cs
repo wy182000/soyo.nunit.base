10f1c3244ce160af3d1ada7d8a0da8f2b0e6b0a4
wy182000 <wy182000@126.com>
2018-04-26 12:48:27 +0800

    modify log.

--- Util/System/SystemInfoTest.cs
+++ Util/System/SystemInfoTest.cs
@@ -2,10 +2,10 @@
 using System.Linq.Expressions;
 using System.Reflection;
 
-using Soyo.Base.Log;
+using Soyo.Base;
 using NUnit.Framework;
 
-namespace UnitTest.Base.Log {
+namespace UnitTest.Base.Util {
   [TestFixture]
   public class SystemInfoTest {
     [Test]
@@ -21,20 +21,20 @@ namespace UnitTest.Base.Log {
     }
 
     public static string TestAssemblyLocationInfoMethod() {
-      return SystemInfo.AssemblyLocationInfo(Assembly.GetCallingAssembly());
+      return SystemInfo.LocationInfo(Assembly.GetCallingAssembly());
     }
 
     [Test]
     public void TestGetTypeFromStringFullyQualified() {
       Type t;
 
-      t = GetTypeFromString("UnitTest.Base.Log.SystemInfoTest", false);
+      t = GetTypeFromString("UnitTest.Base.Util.SystemInfoTest", false);
       Assert.AreSame(typeof(SystemInfoTest), t, "Test explicit case sensitive type load");
 
-      t = GetTypeFromString("UnitTest.Base.Log.SYSTEMINFOTEST", true);
+      t = GetTypeFromString("UnitTest.Base.Util.SYSTEMINFOTEST", true);
       Assert.AreSame(typeof(SystemInfoTest), t, "Test explicit case in-sensitive type load caps");
 
-      t = GetTypeFromString("UnitTest.Base.Log.systeminfotest", true);
+      t = GetTypeFromString("UnitTest.Base.Util.systeminfotest", true);
       Assert.AreSame(typeof(SystemInfoTest), t, "Test explicit case in-sensitive type load lower");
     }
 
@@ -42,13 +42,13 @@ namespace UnitTest.Base.Log {
     public void TestGetTypeFromStringRelative() {
       Type t;
 
-      t = GetTypeFromString("UnitTest.Base.Log.SystemInfoTest", false);
+      t = GetTypeFromString("UnitTest.Base.Util.SystemInfoTest", false);
       Assert.AreSame(typeof(SystemInfoTest), t, "Test explicit case sensitive type load");
 
-      t = GetTypeFromString("UnitTest.Base.Log.SYSTEMINFOTEST", true);
+      t = GetTypeFromString("UnitTest.Base.Util.SYSTEMINFOTEST", true);
       Assert.AreSame(typeof(SystemInfoTest), t, "Test explicit case in-sensitive type load caps");
 
-      t = GetTypeFromString("UnitTest.Base.Log.systeminfotest", true);
+      t = GetTypeFromString("UnitTest.Base.Util.systeminfotest", true);
       Assert.AreSame(typeof(SystemInfoTest), t, "Test explicit case in-sensitive type load lower");
     }
 
@@ -56,15 +56,15 @@ namespace UnitTest.Base.Log {
     public void TestGetTypeFromStringSearch() {
       Type t;
 
-      t = GetTypeFromString("Soyo.Base.Log.SystemInfo", false);
+      t = GetTypeFromString("Soyo.Base.SystemInfo", false);
       Assert.AreSame(typeof(SystemInfo), t,
                                        string.Format("Test explicit case sensitive type load found {0} rather than {1}",
                                                      t.AssemblyQualifiedName, typeof(SystemInfo).AssemblyQualifiedName));
 
-      t = GetTypeFromString("SOYO.BASE.LOG.SYSTEMINFO", true);
+      t = GetTypeFromString("SOYO.BASE.SYSTEMINFO", true);
       Assert.AreSame(typeof(SystemInfo), t, "Test explicit case in-sensitive type load caps");
 
-      t = GetTypeFromString("soyo.base.log.systeminfo", true);
+      t = GetTypeFromString("soyo.base.systeminfo", true);
       Assert.AreSame(typeof(SystemInfo), t, "Test explicit case in-sensitive type load lower");
     }
 
@@ -78,32 +78,32 @@ namespace UnitTest.Base.Log {
 
     [Test]
     public void EqualsIgnoringCase_BothNull_true() {
-      Assert.True(SystemInfo.EqualsIgnoringCase(null, null));
+      Assert.True(SystemInfo.Equals(null, null));
     }
 
     [Test]
     public void EqualsIgnoringCase_LeftNull_false() {
-      Assert.False(SystemInfo.EqualsIgnoringCase(null, "foo"));
+      Assert.False(SystemInfo.Equals(null, "foo"));
     }
 
     [Test]
     public void EqualsIgnoringCase_RightNull_false() {
-      Assert.False(SystemInfo.EqualsIgnoringCase("foo", null));
+      Assert.False(SystemInfo.Equals("foo", null));
     }
 
     [Test]
     public void EqualsIgnoringCase_SameStringsSameCase_true() {
-      Assert.True(SystemInfo.EqualsIgnoringCase("foo", "foo"));
+      Assert.True(SystemInfo.Equals("foo", "foo"));
     }
 
     [Test]
     public void EqualsIgnoringCase_SameStringsDifferentCase_true() {
-      Assert.True(SystemInfo.EqualsIgnoringCase("foo", "FOO"));
+      Assert.True(SystemInfo.Equals("foo", "FOO"));
     }
 
     [Test]
     public void EqualsIgnoringCase_DifferentStrings_false() {
-      Assert.False(SystemInfo.EqualsIgnoringCase("foo", "foobar"));
+      Assert.False(SystemInfo.Equals("foo", "foobar"));
     }
   }
 }
