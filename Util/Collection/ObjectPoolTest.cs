2d4c51b043d67c3461079b7656e984f97d6a5ec3
wy182000 <wy182000@126.com>
2018-10-04 22:51:47 +0800

    modify object pool.

--- Util/Collection/ObjectPoolTest.cs
+++ Util/Collection/ObjectPoolTest.cs
@@ -9,30 +9,35 @@ namespace UnitTest.Base.Util.Collection {
   [TestFixture]
   [Category("Soyo.Base")]
   internal class ObjectPoolTest {
-    private class ObjectPoolable : IPoolable {
+    private class ObjectPoolable {
       public int value = 0;
-      public void Reset() {
-        value = 0;
-      }
     }
 
     private const int CheckCount = 1000;
     [Test]
     public void Test() {
+      int createCount = 0;
       List<ObjectPoolable> objectList = new List<ObjectPoolable>();
       var pool = new ObjectPool<ObjectPoolable>();
+      pool.OnCreate = (o) => createCount++;
+      pool.OnDestroy = (o) => createCount--;
 
       for (int i = 0; i < CheckCount; i++) {
+        pool.OnEnable = (o) => o.value = i;
         var value = pool.Allocate();
-        value.value = i;
+        Assert.AreEqual(value.value, i, "value should be " + i);
+
         objectList.Add(value);
       }
 
       Assert.AreEqual(pool.AllocateCount, CheckCount, "allocate count should be equal to check count.");
+      Assert.AreEqual(pool.CacheCountMax, 0, "cache count max default is 0.");
       Assert.AreEqual(pool.CacheCount, 0, "cache count should be 0.");
+      Assert.AreEqual(createCount, CheckCount, "create count should be equal to check count.");
 
       for (int i = 0; i < CheckCount / 2; i++) {
         var value = objectList[i];
+        pool.OnDisable = (o) => o.value = 0;
         pool.Free(value);
         Assert.AreEqual(value.value, 0);
         objectList[i] = null;
@@ -40,9 +45,16 @@ namespace UnitTest.Base.Util.Collection {
 
       Assert.AreEqual(pool.AllocateCount, CheckCount / 2, "allocate count should be equal to half check count.");
       Assert.AreEqual(pool.CacheCount, CheckCount / 2, "cache count should be equal to half check count.");
+      Assert.AreEqual(createCount, CheckCount, "create count should be equal to check count.");
+
+      pool.CacheCountMax = CheckCount / 4;
+      Assert.AreEqual(pool.CacheCountMax, CheckCount / 4, "cache count max should be equal to quarter check count.");
+      Assert.AreEqual(pool.CacheCount, CheckCount / 4, "cache count should be equal to quarter check count.");
+      Assert.AreEqual(createCount, CheckCount / 2 + CheckCount / 4, "create count should be equal to 3 quarter check count.");
 
       pool.Clear();
       Assert.AreEqual(pool.CacheCount, 0, "cache count should be 0.");
+      Assert.AreEqual(createCount, CheckCount / 2, "create count should be equal to half check count.");
     }
   }
 }
