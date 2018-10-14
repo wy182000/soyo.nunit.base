d84b169d7b02dffea4409a008c58360c3214ff49
wy182000 <wy182000@126.com>
2018-10-14 23:29:26 +0800

    modify serialize for generic type.

--- Util/BufferSerializeTest.cs
+++ Util/BufferSerializeTest.cs
@@ -284,85 +284,140 @@ namespace UnitTest.Base.Util.Serialize {
       public object c;
     }
 
+    private class CheckClassSub {
+      public int A { get; set; }
+      public int B { get; set; }
+      public int C { get; set; }
+      public int a;
+      public int b;
+      public int c;
+      public CheckClassInt ClassInt;
+    }
+
+    private class CheckClassGeneric<T> {
+      public T A { get; set; }
+      public T B { get; set; }
+      public T C { get; set; }
+      public T a;
+      public T b;
+      public T c;
+    }
+
+    private class CheckClassGenericMul<T> {
+      public T ClassGeneric;
+    }
+
+    private void initializeInterface(dynamic value) {
+      value.A = Rand.Default.RandInt();
+      value.B = Rand.Default.RandInt();
+      value.C = Rand.Default.RandInt();
+    }
+
+    private void initializeClass(dynamic value) {
+      initializeInterface(value);
+      value.a = Rand.Default.RandInt();
+      value.b = Rand.Default.RandInt();
+      value.c = Rand.Default.RandInt();
+    }
+
+    private void checkInterfaceValue(dynamic value, dynamic valueSerialized) {
+      Assert.AreEqual(value.A, valueSerialized.A, $"check serialize failed, value A: {value.A}, serialized value A: {valueSerialized.A}");
+      Assert.AreEqual(value.B, valueSerialized.B, $"check serialize failed, value B: {value.B}, serialized value B: {valueSerialized.B}");
+      Assert.AreEqual(value.C, valueSerialized.C, $"check serialize failed, value C: {value.C}, serialized value C: {valueSerialized.C}");
+    }
+
+    private void checkClassValue(dynamic value, dynamic valueSerialized) {
+      checkInterfaceValue(value, valueSerialized);
+      Assert.AreEqual(value.a, valueSerialized.a, $"check serialize failed, value a: {value.a}, serialized value a: {valueSerialized.a}");
+      Assert.AreEqual(value.b, valueSerialized.b, $"check serialize failed, value b: {value.b}, serialized value b: {valueSerialized.b}");
+      Assert.AreEqual(value.c, valueSerialized.c, $"check serialize failed, value c: {value.c}, serialized value c: {valueSerialized.c}");
+    }
+
     private void checkSerializeInterface<T>(dynamic value) {
       var writer = new ByteBufferWriter(BufferSerializer.Size<T>(value));
       BufferSerializer.Write<T>(writer, value);
       dynamic valueSerialized = BufferSerializer.Read<T>(writer.GetBufferReader());
-      Assert.AreEqual(value.A, valueSerialized.A, $"check serialize read failed, value A: {value.A}, serialized value A: {valueSerialized.A}");
-      Assert.AreEqual(value.B, valueSerialized.B, $"check serialize read failed, value B: {value.B}, serialized value B: {valueSerialized.B}");
-      Assert.AreEqual(value.C, valueSerialized.C, $"check serialize read failed, value C: {value.C}, serialized value C: {valueSerialized.C}");
+      checkInterfaceValue(value, valueSerialized);
 
       valueSerialized = BufferSerializer.Merge<T>(writer.GetBufferReader(), default(T));
-      Assert.AreEqual(value.A, valueSerialized.A, $"check serialize merge failed, value A: {value.A}, serialized value A: {valueSerialized.A}");
-      Assert.AreEqual(value.B, valueSerialized.B, $"check serialize merge failed, value B: {value.B}, serialized value B: {valueSerialized.B}");
-      Assert.AreEqual(value.C, valueSerialized.C, $"check serialize merge failed, value C: {value.C}, serialized value C: {valueSerialized.C}");
+      checkInterfaceValue(value, valueSerialized);
     }
 
     private void checkSerializeClass<T>(dynamic value) {
       var writer = new ByteBufferWriter(BufferSerializer.Size<T>(value));
       BufferSerializer.Write<T>(writer, value);
       dynamic valueSerialized = BufferSerializer.Read<T>(writer.GetBufferReader());
-      Assert.AreEqual(value.A, valueSerialized.A, $"check serialize read failed, value A: {value.A}, serialized value A: {valueSerialized.A}");
-      Assert.AreEqual(value.a, valueSerialized.a, $"check serialize read failed, value a: {value.a}, serialized value a: {valueSerialized.a}");
-      Assert.AreEqual(value.B, valueSerialized.B, $"check serialize read failed, value B: {value.B}, serialized value B: {valueSerialized.B}");
-      Assert.AreEqual(value.b, valueSerialized.b, $"check serialize read failed, value b: {value.b}, serialized value b: {valueSerialized.b}");
-      Assert.AreEqual(value.C, valueSerialized.C, $"check serialize read failed, value C: {value.C}, serialized value C: {valueSerialized.C}");
-      Assert.AreEqual(value.c, valueSerialized.c, $"check serialize read failed, value c: {value.c}, serialized value c: {valueSerialized.c}");
+      checkClassValue(value, valueSerialized);
 
       valueSerialized = BufferSerializer.Merge<T>(writer.GetBufferReader(), default(T));
-      Assert.AreEqual(value.A, valueSerialized.A, $"check serialize merge failed, value A: {value.A}, serialized value A: {valueSerialized.A}");
-      Assert.AreEqual(value.a, valueSerialized.a, $"check serialize merge failed, value a: {value.a}, serialized value a: {valueSerialized.a}");
-      Assert.AreEqual(value.B, valueSerialized.B, $"check serialize merge failed, value B: {value.B}, serialized value B: {valueSerialized.B}");
-      Assert.AreEqual(value.b, valueSerialized.b, $"check serialize merge failed, value b: {value.b}, serialized value b: {valueSerialized.b}");
-      Assert.AreEqual(value.C, valueSerialized.C, $"check serialize merge failed, value C: {value.C}, serialized value C: {valueSerialized.C}");
-      Assert.AreEqual(value.c, valueSerialized.c, $"check serialize merge failed, value c: {value.c}, serialized value c: {valueSerialized.c}");
+      checkClassValue(value, valueSerialized);
+    }
+
+    private void checkSerializeClassSub<T>(dynamic value) {
+      var writer = new ByteBufferWriter(BufferSerializer.Size<T>(value));
+      BufferSerializer.Write<T>(writer, value);
+      dynamic valueSerialized = BufferSerializer.Read<T>(writer.GetBufferReader());
+      checkClassValue(value, valueSerialized);
+      checkClassValue(value.ClassInt, valueSerialized.ClassInt);
+
+      valueSerialized = BufferSerializer.Merge<T>(writer.GetBufferReader(), default(T));
+      checkClassValue(value, valueSerialized);
+      checkClassValue(value.ClassInt, valueSerialized.ClassInt);
+    }
+
+    private void checkSerializeGenericMul<T>(dynamic value) {
+      var writer = new ByteBufferWriter(BufferSerializer.Size<T>(value));
+      BufferSerializer.Write<T>(writer, value);
+      dynamic valueSerialized = BufferSerializer.Read<T>(writer.GetBufferReader());
+      checkClassValue(value.ClassGeneric, valueSerialized.ClassGeneric);
+
+      valueSerialized = BufferSerializer.Merge<T>(writer.GetBufferReader(), default(T));
+      checkClassValue(value.ClassGeneric, valueSerialized.ClassGeneric);
     }
 
     [Test]
     public void TestSerializeClass() {
       var intClass = new CheckClassInt();
-      intClass.A = Rand.Default.RandInt();
-      intClass.a = Rand.Default.RandInt();
-      intClass.B = Rand.Default.RandInt();
-      intClass.b = Rand.Default.RandInt();
-      intClass.C = Rand.Default.RandInt();
-      intClass.c = Rand.Default.RandInt();
+      initializeClass(intClass);
       checkSerializeClass<CheckClassInt>(intClass);
       checkSerializeClass<object>(intClass);
       checkSerializeInterface<ICheckInt>(intClass);
 
       var objectClass = new CheckClassObject();
-      objectClass.A = Rand.Default.RandInt();
-      objectClass.a = Rand.Default.RandInt();
-      objectClass.B = Rand.Default.RandInt();
-      objectClass.b = Rand.Default.RandInt();
-      objectClass.C = Rand.Default.RandInt();
-      objectClass.c = Rand.Default.RandInt();
+      initializeClass(objectClass);
       checkSerializeClass<CheckClassObject>(objectClass);
       checkSerializeClass<object>(objectClass);
       checkSerializeInterface<ICheckObject>(objectClass);
 
       var intStruct = new CheckStructInt();
-      intStruct.A = Rand.Default.RandInt();
-      intStruct.a = Rand.Default.RandInt();
-      intStruct.B = Rand.Default.RandInt();
-      intStruct.b = Rand.Default.RandInt();
-      intStruct.C = Rand.Default.RandInt();
-      intStruct.c = Rand.Default.RandInt();
+      initializeClass(intStruct);
       checkSerializeClass<CheckStructInt>(intStruct);
       checkSerializeClass<object>(intStruct);
       checkSerializeInterface<ICheckInt>(intStruct);
 
       var objectStruct = new CheckStructObject();
-      objectStruct.A = Rand.Default.RandInt();
-      objectStruct.a = Rand.Default.RandInt();
-      objectStruct.B = Rand.Default.RandInt();
-      objectStruct.b = Rand.Default.RandInt();
-      objectStruct.C = Rand.Default.RandInt();
-      objectStruct.c = Rand.Default.RandInt();
+      initializeClass(objectStruct);
       checkSerializeClass<CheckStructObject>(objectStruct);
       checkSerializeClass<object>(objectStruct);
       checkSerializeInterface<ICheckObject>(objectStruct);
+
+      var subClass = new CheckClassSub();
+      initializeClass(subClass);
+      subClass.ClassInt = new CheckClassInt();
+      initializeClass(subClass.ClassInt);
+      checkSerializeClassSub<CheckClassSub>(subClass);
+      checkSerializeClassSub<object>(subClass);
+
+      var genericClass = new CheckClassGeneric<int>();
+      initializeClass(genericClass);
+      checkSerializeClass<CheckClassGeneric<int>>(genericClass);
+      checkSerializeClass<object>(genericClass);
+
+      var genericClassMul = new CheckClassGenericMul<CheckClassGeneric<int>>();
+      genericClassMul.ClassGeneric = new CheckClassGeneric<int>();
+      initializeClass(genericClassMul.ClassGeneric);
+      checkSerializeGenericMul<CheckClassGenericMul<CheckClassGeneric<int>>>(genericClassMul);
+      checkSerializeGenericMul<object>(genericClassMul);
 
       checkSerializeValue<CheckClassInt>(null);
       checkSerializeValue<CheckClassObject>(null);
